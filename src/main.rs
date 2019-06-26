2448e146242493138a5142fcd76fa90edea464d8
statiolake <statiolake@gmail.com>
2019-06-27 03:15:37 +0900

    Support dependencies specified by git revision

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use cargo::core::manifest::{EitherManifest, Manifest};
-use cargo::core::SourceId;
+use cargo::core::{Dependency as CargoDependency, GitReference, SourceId};
 use cargo::util::config::Config;
 use cargo::util::toml::read_manifest;
 use std::env;
@@ -30,22 +30,43 @@ struct Dependency {
 }
 
 impl Dependency {
-    pub fn parse(
-        deps_path: &Path,
-        package_name: String,
-        version_req: String,
-    ) -> Result<Dependency> {
-        if !deps_path.exists() {
-            return Err("dependencies path is not exist.".into());
+    fn parse_git(package_name: String, git_ref: &GitReference) -> Result<Locator> {
+        match git_ref {
+            GitReference::Rev(revision) => Ok(Locator::Git {
+                package_name,
+                revision: revision.clone(),
+            }),
+            GitReference::Tag(_) => panic!("Tagged git source is not supported."),
+            GitReference::Branch(_) => panic!("Branch git source is not supported."),
         }
+    }
 
+    fn parse_normal(package_name: String, version_req: String) -> Result<Locator> {
         if !version_req.starts_with('=') {
             return Err("use exact match version requirement: `= *.*.*`".into());
         }
 
         let version = version_req[1..].trim().to_string();
 
-        let library_path = find_library_path(deps_path, &package_name, &version)?;
+        Ok(Locator::Version {
+            package_name,
+            version,
+        })
+    }
+
+    pub fn parse(deps_path: &Path, dep: &CargoDependency) -> Result<Dependency> {
+        if !deps_path.exists() {
+            return Err("dependencies path is not exist.".into());
+        }
+
+        let package_name = dep.package_name().to_string();
+
+        let locator = match dep.source_id().git_reference() {
+            Some(git_ref) => Dependency::parse_git(package_name.clone(), git_ref),
+            None => Dependency::parse_normal(package_name.clone(), dep.version_req().to_string()),
+        }?;
+
+        let library_path = locator.find_library_path(deps_path)?;
 
         Ok(Dependency {
             package_name,
@@ -62,46 +83,94 @@ impl Dependency {
     }
 }
 
-fn find_library_path(deps_path: &Path, package_name: &str, version: &str) -> Result<PathBuf> {
-    //
-    // FIXME: maybe more better way
-    //
-    // This function uses `{crate_name}-{random}.d` file in
-    // /target/release/deps dir.  This file seems to contain paths for files
-    // included in that crate.  Usually dependency crates that cargo pulled from
-    // crates.io is placed under
-    // ~/.cargo/registory/github-{random}/{package_name}-{version}/, so by
-    // looking at `*.d` file we can determine the correct version of library
-    // file.
-    //
+enum Locator {
+    Version {
+        package_name: String,
+        version: String,
+    },
+    Git {
+        package_name: String,
+        revision: String,
+    },
+}
 
-    let crate_name = package_name.replace("-", "_");
-    let package_string = format!("/{}-{}/", package_name, version);
-
-    for file in deps_path.read_dir()? {
-        let file = file?;
-        if file.file_type()?.is_dir() {
-            continue;
-        }
-
-        let file_name = file.file_name();
-        let file_name = file_name
-            .to_str()
-            .ok_or("file_name has invalid byte for UTF-8")?;
-        if !file_name.starts_with(&crate_name) {
-            continue;
-        }
-
-        let path = to_absolute::to_absolute_from_current_dir(file.path())?;
-        let content = fs::read_to_string(&path)?;
-        if content.contains(&package_string) {
-            return Ok(path
-                .with_file_name(format!("lib{}", file_name))
-                .with_extension("rlib"));
+impl Locator {
+    fn package_name(&self) -> &str {
+        match self {
+            Locator::Version { package_name, .. } => package_name,
+            Locator::Git { package_name, .. } => package_name,
         }
     }
 
-    Err(format!("failed to find appropriate path for {}", package_string).into())
+    fn crate_name(&self) -> String {
+        self.package_name().replace("-", "_")
+    }
+
+    fn search_patterns(&self) -> Vec<String> {
+        //
+        // FIXME: maybe more better way
+        //
+        // This function uses `{crate_name}-{random}.d` file in
+        // /target/release/deps dir.  This file seems to contain paths for files
+        // included in that crate.  Usually dependency crates that cargo pulled from
+        // crates.io is placed under
+        // ~/.cargo/registory/github-{random}/{package_name}-{version}/, so by
+        // looking at `*.d` file we can determine the correct version of library
+        // file.
+        //
+        //
+        match self {
+            Locator::Version {
+                package_name,
+                version,
+            } => vec![format!("/{}-{}/", package_name, version)],
+            Locator::Git {
+                package_name,
+                revision,
+            } => vec![
+                format!("/{}", package_name),
+                format!("/{}/", &revision[0..7]),
+            ],
+        }
+    }
+
+    fn matches(&self, content: String) -> bool {
+        self.search_patterns()
+            .iter()
+            .all(|pat| content.contains(pat))
+    }
+
+    fn find_library_path(&self, deps_path: &Path) -> Result<PathBuf> {
+        let crate_name = self.crate_name();
+        for file in deps_path.read_dir()? {
+            let file = file?;
+            if file.file_type()?.is_dir() {
+                continue;
+            }
+
+            let file_name = file.file_name();
+            let file_name = file_name
+                .to_str()
+                .ok_or("file_name has invalid byte for UTF-8")?;
+            if !file_name.starts_with(&crate_name) {
+                continue;
+            }
+
+            let path = to_absolute::to_absolute_from_current_dir(file.path())?;
+            let content = fs::read_to_string(&path)?;
+            if self.matches(content) {
+                return Ok(path
+                    .with_file_name(format!("lib{}", file_name))
+                    .with_extension("rlib"));
+            }
+        }
+
+        Err(format!(
+            "failed to find appropriate path for {}",
+            self.package_name()
+        )
+        .into())
+    }
 }
 
 fn main() -> Result<()> {
@@ -118,13 +187,7 @@ fn main() -> Result<()> {
     let options = manifest
         .dependencies()
         .iter()
-        .map(|dep| {
-            Dependency::parse(
-                &deps_path,
-                dep.package_name().to_string(),
-                dep.version_req().to_string(),
-            )
-        })
+        .map(|dep| Dependency::parse(&deps_path, dep))
         .collect::<Result<Vec<_>>>()?
         .into_iter()
         .flat_map(|dep| dep.make_compile_option())
